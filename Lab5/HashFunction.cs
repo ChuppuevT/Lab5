51c09c43925911c4dc0d26f1b23dbacad910517d
tim.chuppuev@mail.ru <tim.chuppuev@mail.ru>
2021-05-21 04:51:49 +0400

    Добавьте файлы проекта.

---
+++ Lab5/HashFunction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace Lab5
+{
+    internal static class HashFunction
+    {
+        public static int Hash(string value)
+        {
+            int p_pow = 1;
+            const int p = 31;
+
+            long h = 0L;
+
+            foreach (var s in value)
+            {
+                h += (s - 'a' + 1) * p_pow;
+                p_pow *= p;
+            }
+
+            return (int) Math.Abs(h % CountElements);
+        }
+
+        public static int CountElements { get; set; }
+    }
+}
